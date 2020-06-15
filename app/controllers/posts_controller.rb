1a521fce8e80bd0202fe636a16f7c15ce66649e9
codetalkertive <codetalkertive@gmail.com>
2020-06-15 14:02:54 +0100

    Add Create and Show Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,32 @@ class PostsController < ApplicationController
   end
 
   def new
+  	@post = Post.new
+  end
+
+  def create
+
+  	@post = Post.create(post_params)
+
+  	if @post.save
+  		redirect_to @post
+  	else
+  		render :new
+  	end
   end
 
   def show
+  	@post = Post.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def post_params
+  	params.require(:post).permit(:title, :content)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
+
   get 'posts/index'
-  get 'posts/new'
-  get 'posts/show'
-  get 'posts/edit'
+  root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
