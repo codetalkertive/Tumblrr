37732cfca47f7105cf8ddc82bdd610e1b3afa3ef
codetalkertive <codetalkertive@gmail.com>
2020-06-15 14:09:13 +0100

    Add Edit and Delete Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,24 @@ class PostsController < ApplicationController
   end
 
   def edit
+  	@post = Post.find(params[:id])
+  end
+
+  def update
+  	@post = Post.find(params[:id])
+
+  	if @post.update(post_params)
+  		redirect_to @post
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	@post = Post.find(params[:id])
+
+  	@post.destroy
+  	redirect_to :action => :index
   end
 
   private
