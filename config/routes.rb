3c1fa7afc0bbde62c1439eadf9793b6d3dad408b
codetalkertive <codetalkertive@gmail.com>
2020-06-23 13:21:36 +0100

    Add Devise and User

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 	
+	devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+end
   
+  devise_for :users
   resources :posts do
   	resources :comments
   end
